da67669a5f4299f81dcd0dd1ca0be2aa2e4753ce
TPerronTouchette <113641343+TPerronTouchette@users.noreply.github.com>
2023-09-21 10:41:46 -0400

    ajout de classe de test

--- Test/TestPieces.py
+++ Test/TestPieces.py
@@ -2,6 +2,10 @@ import unittest
 from pieces.piece import Piece
 from pieces.pion import Pion
 from pieces.cavalier import Cavalier
+from pieces.fou import Fou
+from pieces.roi import Roi
+from pieces.reine import Reine
+from pieces.tour import Tour
 
 class TestPiece(unittest.TestCase):
     #__test__ = False
@@ -29,17 +33,7 @@ class TestPion(TestPiece, unittest.TestCase):
     def set_up(self):
         self.piece = Pion("Noir", (1,0))
 
-    """
-    def testTypeGetMouvement(self):
-        piece = Pion("Noir", (1, 0))
-        mouvements = piece.getMouvement()
-        self.assertTrue(isinstance(mouvements, list), "getMouvement() ne retourne pas une liste ")
-        self.assertTrue(isinstance(mouvements[0], list), "getMouvement() ne retourne pas une liste de liste")
-        self.assertTrue(isinstance(mouvements[0][0], tuple), "getMouvement() ne retourne pas une liste de liste de tuple")
-        self.assertTrue(isinstance(mouvements[0][0][0], int), "getMouvement() ne retourne pas une liste de liste de tuple de 2 int")
-        self.assertTrue(isinstance(mouvements[0][0][1], int), "getMouvement() ne retourne pas une liste de liste de tuple de 2 int")
-        self.assertEqual(len(mouvements[0][0]), 2, "getMouvement() ne retourne pas une liste de liste de tuple de 2 int")
-    """
+
     def testGetMouvementsNoir(self):
         piece = Pion("Noir", (1, 0))
         mouvements = piece.getMouvement()
@@ -51,12 +45,62 @@ class TestPion(TestPiece, unittest.TestCase):
         self.assertListEqual(mouvements, [[(5,0), (4,0)], [(5, 1), (5, -1)]])
 
 class TestCavalier(TestPiece, unittest.TestCase):
+
     def set_up(self):
         self.piece = Cavalier("Noir", (4,4))
 
     def testGetMouvements(self):
         self.set_up()
+        mouvementsExpected = [[(2,3)],[(2,5)],[(3,2)],[(3,6)],[(5,2)],[(5,6)],[(6,3)],[(6,5)]]
+        mouvements = self.piece.getMouvement()
+        self.assertListEqual(mouvements, mouvementsExpected, "Les mouvements ne correspondent pas à un cavalier")
 
+class TestFou(TestPiece, unittest.TestCase):
+    def set_up(self):
+        self.piece = Fou("Noir", (4,4))
 
+    def testGetMouvements(self):
+        self.set_up()
+        mouvementsExpected = [[(3,3), (2, 2), (1, 1), (0, 0)],[(2,5)],[(3,2)],[(3,6)],[(5,2)],[(5,6)],[(6,3)],[(6,5)]]
+        mouvements = self.piece.getMouvement()
+        for i in mouvements:
+            estComplet = True
+            j = i[0]
+
+            for k in mouvementsExpected:
+                bonneSousListe = True
+
+                if j != k[0]:
+                    bonneSousListe = False
+                    estComplet = False
+        self.assertListEqual(mouvements, mouvementsExpected, "Les mouvements ne correspondent pas à un cavalier")
+class TestReine(TestPiece, unittest.TestCase):
+    def set_up(self):
+        self.piece = Reine("Noir", (4,4))
+
+    def testGetMouvements(self):
+        self.set_up()
+        mouvementsExpected = [[(2,3)],[(2,5)],[(3,2)],[(3,6)],[(5,2)],[(5,6)],[(6,3)],[(6,5)]]
+        mouvements = self.piece.getMouvement()
+        self.assertListEqual(mouvements, mouvementsExpected, "Les mouvements ne correspondent pas à un cavalier")
+class TestRoi(TestPiece, unittest.TestCase):
+    def set_up(self):
+        self.piece = Roi("Noir", (4,4))
+
+    def testGetMouvements(self):
+        self.set_up()
+        mouvementsExpected = [[(2,3)],[(2,5)],[(3,2)],[(3,6)],[(5,2)],[(5,6)],[(6,3)],[(6,5)]]
+        mouvements = self.piece.getMouvement()
+        self.assertListEqual(mouvements, mouvementsExpected, "Les mouvements ne correspondent pas à un cavalier")
+class TestTour(TestPiece, unittest.TestCase):
+    def set_up(self):
+        self.piece = Tour("Noir", (4,4))
+
+    def testGetMouvements(self):
+        self.set_up()
+        mouvementsExpected = [[(2,3)],[(2,5)],[(3,2)],[(3,6)],[(5,2)],[(5,6)],[(6,3)],[(6,5)]]
+        mouvements = self.piece.getMouvement()
+
+        self.assertListEqual(mouvements, mouvementsExpected, "Les mouvements ne correspondent pas à un cavalier")
 if __name__ == '__main__':
     unittest.main()
